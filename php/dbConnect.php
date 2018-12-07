b325867c3935379c7005b812eb051a9f4397fea0
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2018-12-07 21:33:51 +0700

    initial commit

---
+++ php/dbConnect.php
@@ -0,0 +1,10 @@
+<?php
+
+define('HOST','localhost');
+define('USER','u3730452');
+define('PASS','');
+define('DB','u3730452_SayurData');
+
+//Connecting to Database
+$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+?>
