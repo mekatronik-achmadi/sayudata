c3da1868b2043cacf72cd0d268d9e65a320b5aca
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-01-25 07:06:26 +0000

    admin app first

---
+++ app/syrAdmin/src/com/example/syrAdmin/ServerConst.java
@@ -0,0 +1,17 @@
+package com.example.syrAdmin;
+
+/**
+ * Created by farm on 1/25/19.
+ */
+public class ServerConst {
+    public static final String URL_COMO_ADD="add_Como.php";
+    public static final String URL_COMO_DEL="del_Como.php";
+    public static final String URL_COMO_GET="get_Como.php";
+    public static final String URL_COMO_LIST="list_Como.php";
+    public static final String URL_COMO_UPD="upd_Como.php";
+
+    public static final String KEY_COMO_ID = "id";
+    public static final String KEY_COMO_SAYUR = "sayur";
+
+    public static final String ID_COMO = "id_como";
+}
