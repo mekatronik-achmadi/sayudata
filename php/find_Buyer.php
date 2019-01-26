6c87f12932097fa1ce2e1ca73028c0526293260c
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-01-26 12:45:57 +0000

    find buyer

---
+++ php/find_Buyer.php
@@ -0,0 +1,24 @@
+<?php
+
+	$name = $_GET['name'];
+
+	require_once('db_Connect.php');
+
+	$sql = "SELECT * FROM penjual WHERE name LIKE '%$name%'";
+
+	$r = mysqli_query($con,$sql);
+
+	$result = array();
+
+	while($row = mysqli_fetch_array($r)){
+
+		array_push($result,array(
+			"id"=>$row['id'],
+			"name"=>$row['name']
+		));
+	}
+
+	echo json_encode(array('result'=>$result));
+
+	mysqli_close($con);
+?>
