7af6150df2e58454220cc3d7d4ec6c64e17d0293
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-01-26 05:32:33 +0000

    list oke

---
+++ php/find_Como.php
@@ -0,0 +1,21 @@
+<?php
+
+	$sayur = $_GET['sayur'];
+
+	require_once('db_Connect.php');
+
+	$sql = "SELECT * FROM komoditi WHERE sayur=$sayur";
+
+	$r = mysqli_query($con,$sql);
+
+	$result = array();
+	$row = mysqli_fetch_array($r);
+	array_push($result,array(
+			"id"=>$row['id'],
+			"sayur"=>$row['sayur'],
+		));
+
+	echo json_encode(array('result'=>$result));
+
+	mysqli_close($con);
+?>
