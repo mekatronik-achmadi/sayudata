20c6a91121b0ed2b21ba9932a1d12a73bf3bcc7d
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-01-29 08:37:42 +0000

    start working on consumer app

--- app/syrAdmin/src/com/example/syrAdmin/MainActivity.java
+++ app/syrAdmin/src/com/example/syrAdmin/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.syrAdmin;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TabHost;
 
 public class MainActivity extends TabActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends TabActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
 
         TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
