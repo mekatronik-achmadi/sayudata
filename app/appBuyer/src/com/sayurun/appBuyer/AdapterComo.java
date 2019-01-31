46cc6cc16cba28baee2dd4ded7e059bde5b0492f
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-01-31 14:14:44 +0000

    drop imageURL and use Picasso instead

--- app/appBuyer/src/com/sayurun/appBuyer/AdapterComo.java
+++ app/appBuyer/src/com/sayurun/appBuyer/AdapterComo.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.squareup.picasso.Picasso;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -36,52 +37,6 @@ public class AdapterComo extends BaseAdapter{
         ImageView imgVw;
     }
 
-    private class GetImgURL extends AsyncTask<String,Void,Bitmap>{
-        ImageView imgView;
-
-        public  GetImgURL(ImageView imgView){
-            this.imgView = imgView;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bmpres) {
-            super.onPostExecute(bmpres);
-            imgView.setImageBitmap(bmpres);
-        }
-
-        @Override
-        protected Bitmap doInBackground(String... urls){
-            String imgurl = urls[0];
-            Bitmap bmp = null;
-
-            try{
-                URL url = new URL(imgurl);
-                HttpURLConnection.setFollowRedirects(false);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(10000);
-                conn.setConnectTimeout(10000);
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-
-                if(conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                    InputStream in = conn.getInputStream();
-                    bmp = BitmapFactory.decodeStream(in);
-                }else{
-                    bmp = BitmapFactory.decodeResource(mContext.getResources(),R.drawable.ic_launcher);
-                }
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-            return bmp;
-        }
-    }
-
     public View getView(int position, View convertView, ViewGroup parent){
 
         ViewCell cell;
@@ -103,7 +58,7 @@ public class AdapterComo extends BaseAdapter{
         cell.nidVw.setText(nidString.get(position));
         cell.txtVw.setText(itemString.get(position));
         cell.imgurlVw.setText(imgStrURL.get(position));
-        new GetImgURL(cell.imgVw).execute(imgStrURL.get(position));
+        Picasso.with(mContext).load(imgStrURL.get(position)).into(cell.imgVw);
         cell.imgVw.getLayoutParams().height = 150;
 
         return convertView;
