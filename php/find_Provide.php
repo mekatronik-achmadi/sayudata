3591f75eb3906a01b3819752e1549f117ee2f560
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-01-31 02:21:43 +0000

    find provide

---
+++ php/find_Provide.php
@@ -0,0 +1,29 @@
+<?php
+	$sayur = $_GET['sayur'];
+
+	require_once('db_Connect.php');
+
+	$sql = "SELECT * FROM sedia WHERE sayur LIKE '%$sayur%'";
+
+	$r = mysqli_query($con,$sql);
+
+	$result = array();
+
+	while($row = mysqli_fetch_array($r)){
+
+		array_push($result,array(
+			"id"=>$row['id'],
+			"seller"=>$row['seller'],
+			"tanggal"=>$row['tanggal'],
+			"sayur"=>$row['sayur'],
+			"stok"=>$row['stok'],
+			"harga"=>$row['harga'],
+			"satuan"=>$row['satuan'],
+			"area"=>$row['area']
+		));
+	}
+
+	echo json_encode(array('result'=>$result));
+
+	mysqli_close($con);
+?>
