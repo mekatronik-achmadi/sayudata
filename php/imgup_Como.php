9aced41fc0e6c6d43234e38548a8ac621e10978c
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-01-31 01:41:04 +0000

    image uploader

--- php/imgup_Como.php
+++ php/imgup_Como.php
@@ -7,38 +7,38 @@
 	if(isset($_POST["submit"])){
 		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 		if($check !== false){
-			echo "File is an image".$check["mime"].". \r\n";
+			echo "File is an ".$check["mime"].". <br>";
 			$uploadOk = 1;
 		}
 		else{
-			echo "File not an image \r\n";
+			echo "File not an image <br>";
 			$uploadOk = 0;
 		}
 	}
 	
 	if(file_exists($targetfile)){
-		echo "File already exist. Overwriting \r\n";
+		echo "File already exist. Overwriting <br>";
 	}
 	
 	if($_FILES["fileToUpload"]["size"] > 30000){
-		echo "Sorry your file too big \r\n";
+		echo "Sorry your file too big <br>";
 		$uploadOk = 0;
 	}
 	
 	if($imageFileType != "jpg"){
-		echo "Sorry your image is not JPG \r\n";
+		echo "Sorry your image is not JPG <br>";
 		$uploadOk = 0;	
 	}
 	
 	if($uploadOk == 0){
-		echo "Your image not uploaded \r\n";
+		echo "Your image not uploaded <br>";
 	}
 	else{
 		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$targetfile)){
-			echo "File ".basename($_FILES["fileToUpload"]["name"])." uploaded \r\n";
+			echo "File ".basename($_FILES["fileToUpload"]["name"])." uploaded <br>";
 		}
 		else{
-			echo "An error happen while uploading \r\n";
+			echo "An error happen while uploading <br>";
 		}
 	}
 	
