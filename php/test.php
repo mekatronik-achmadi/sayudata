f7801abb076f46d2700fe50f818d17f3f809eb90
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-02-14 12:25:57 +0700

    test layout

--- php/test.php
+++ php/test.php
@@ -1,7 +1,32 @@
 <!DOCTYPE html>
+	
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
+		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+		<style>
+			body {
+				font-family: Arial;
+				color: black;
+			}
+			
+			.split {
+				height: 100%;
+				width: 50%;
+				position: fixed;
+				z-index: 1;
+				top: 0;
+				overflow-x: hidden;
+				padding-top: 20px;
+			}
+			  
+			.left {
+				left: 0;
+			}
+			  
+			.right {
+				right: 0;
+			}
+		</style>
 		<title>Manage Sayuran Form</title>
 	</head>
 	
