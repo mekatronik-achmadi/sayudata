006121af29c24331683dcfc29ee8ab2c9450f624
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-02-14 12:53:35 +0700

    test layout

--- php/test.php
+++ php/test.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8" content="width=device-width, initial-scale=1">
+		<meta charset="UTF-8">
 		<title>Manage Sayuran Form</title>
 		<style>
 			* {
